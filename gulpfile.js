df9dcac73a090f4590ddeefd8675ee03196fafc6
Karina Zhuro <kzhuro36@gmail.com>
2019-11-18 17:55:29 +0300

    Plug in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
-var gulp = require('gulp'), // Подключаем Gulp
-    sass = require('gulp-sass'); // Подключаем Sass пакет
-
+let gulp = require('gulp');
+let sass = require('gulp-sass');
 gulp.task('sass', function() { // Создаем таск "sass"
-    return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
+    return gulp.src('app/sass/**/*.sass', 'app/sass/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
-
 gulp.task('watch', function() {
-    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/sass/**/*.sass', 'app/sass/**/*.scss', gulp.parallel('sass'));
 });
-
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.parallel('sass', 'watch'));
