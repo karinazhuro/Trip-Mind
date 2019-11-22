ab61ce449f55201f5a44e25a008212cb7a84dea0
Karina Zhuro <kzhuro36@gmail.com>
2019-11-22 15:16:59 +0300

    Layout main screen

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 gulp.task('sass', function() { // Создаем таск "sass"
-    return gulp.src('app/sass/**/*.sass', 'app/sass/**/*.scss') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    return gulp.src('app/sass/**/*.scss') // Берем источник
+        .pipe(sass.sync().on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
 gulp.task('watch', function() {
-    gulp.watch('app/sass/**/*.sass', 'app/sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
 });
 gulp.task('default', gulp.parallel('sass', 'watch'));
